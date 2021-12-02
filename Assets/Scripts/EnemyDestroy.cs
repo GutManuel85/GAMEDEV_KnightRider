46dafbd0aee96779b89625d99cd73b6424066ca9
Nikola Momcilovic <nikola.momcilovic@stud.hslu.ch>
2021-12-02 22:44:32 +0100

    Bugfix shooting and added sound + juiciness with particicle system

--- Assets/Scripts/EnemyDestroy.cs
+++ Assets/Scripts/EnemyDestroy.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 {
+    public AudioClip audio;
+    public ParticleSystem ps;
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "BulletShoot(Clone)") Destroy(gameObject);
+        if (collision.gameObject.name == "BulletShoot(Clone)")
+        {
+            AudioSource.PlayClipAtPoint(audio, this.gameObject.transform.position);
+
+            ParticleSystem a = Instantiate(ps, new Vector3(gameObject.transform.position.x, 1.5f, gameObject.transform.position.z), Quaternion.identity);
+            a.Play();
+            Destroy(gameObject);
+        }
     }
 }
