add28a4fc3cb6da5c21977aa41bafcffaff5e11e
Colin Dexheimer <code>
2021-12-04 11:10:51 +0100

    fix lighting

--- Assets/Scripts/GameRestarter.cs
+++ Assets/Scripts/GameRestarter.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameRestarter : MonoBehaviour
 {
-
-    public Light environmentLight;
-    private float lightIntensitiy;
-
-    void Start()
-    {
-        lightIntensitiy = environmentLight.intensity;
-    }
 
     // Update is called once per frame
     void Update()
@@ -20,7 +10,6 @@ public class GameRestarter : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            environmentLight.intensity = 1;
             Time.timeScale = 1;
         }
     }
